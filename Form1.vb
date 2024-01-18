2e2ecf2abe47ba5e41b651fd5ab7ab82d88d1913
Ramavhale Murendeni <Ramavhale Murendeni@DESKTOP-CO1SVRB>
2024-01-18 22:33:22 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim firtNumber, secondNumber As Integer
+        firtNumber = Val(TextBox1.Text)
+        secondNumber = Val(TextBox2.Text)
+
+        Sum(firtNumber, secondNumber)
+
+    End Sub
+
+    Sub Sum(a As Integer, b As Integer)
+        Dim total = a + b
+
+        total = a + b
+
+        TextBox3.Text = total
+
+
+
+    End Sub
+End Class
